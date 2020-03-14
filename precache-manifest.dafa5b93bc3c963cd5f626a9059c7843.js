860e152ced8c9a366452fe423219830c6c12edbf
Ioan.Ghisoi <ioan.ghisoi@johnnys-mac.mynet>
2020-03-14 22:21:31 +0000

    Updates

---
+++ precache-manifest.dafa5b93bc3c963cd5f626a9059c7843.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aa53f81fef6e673ca705bae671705e1",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "022a564a3c1249c626f1",
+    "url": "/monsters/static/css/main.525f5e1b.chunk.css"
+  },
+  {
+    "revision": "34fc867681234f211387",
+    "url": "/monsters/static/js/2.a828a9b9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters/static/js/2.a828a9b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "022a564a3c1249c626f1",
+    "url": "/monsters/static/js/main.d0df177e.chunk.js"
+  },
+  {
+    "revision": "08c07ceb1e48d12021a1",
+    "url": "/monsters/static/js/runtime-main.4a77be07.js"
+  }
+]);
